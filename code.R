94dc233f9e98706d63446f83ffb9f27fb9af0c26
felipelbhering <felipelbhering@gmail.com>
2018-08-04 00:04:06 -0300

    updating code all DF rbindlist

--- code.R
+++ code.R
@@ -2,6 +2,8 @@ list.of.packages <- c("rjson")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
+library(rjson)
+
 #functions
 ###############
 write_table_clipboard <-function(df, rownames = FALSE, dec = '.', sep = '\t'){	
@@ -22,17 +24,14 @@ write_table_file<-function(name, df, rownames = FALSE, dec = '.', sep = '\t'){
 json_file <- "wunderlist-2018083-20_52_08.json"
 json_data <- fromJSON(paste(readLines(json_file), collapse=""))
 
-#write_table_clipboard (a)
 
 list.condition <- sapply(json_data$data$tasks, function(x) x$completed == "FALSE")
 output.list  <- json_data$data$tasks[list.condition]
 
-#length(json_data$data$tasks)
-#length(output.list)
 
-a=lapply(json_data$data$tasks, `[[`, 'title')
-a2=lapply(output.list, `[[`, 'title')
+#a2 <- lapply(output.list, `[[`, 'title')
+df <- rbind_list(output.list)
+df <- df %>% select(-c(created_by_id,id,completed,type))
 
-
-write_table_file(a2,name='lista.txt')
+write_table_file(df, name='lista.txt')
 
