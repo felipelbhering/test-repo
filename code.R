86ee7f7d532a8b5719d664d3e18bcc0b96d2df85
felipelbhering <felipelbhering@gmail.com>
2018-08-04 00:19:55 -0300

    new changes and feature selection

--- code.R
+++ code.R
@@ -1,7 +1,7 @@
 
 ###############
 #Install packages 
-list.of.packages <- c("rjson", "dplyr", "magrittr")
+list.of.packages <- c("rjson", "dplyr", "magrittr", "stringr")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -9,6 +9,7 @@ if(length(new.packages)) install.packages(new.packages)
 library(rjson)
 library(dplyr)
 library(magrittr)
+library(stringr)
 
 #functions
 ###############
@@ -38,8 +39,11 @@ output.list  <- json_data$data$tasks[list.condition]
 
 
 #a2 <- lapply(output.list, `[[`, 'title')
-df <- rbind_list(output.list)
-df <- df %>% select(-c(created_by_id, id, completed, type))
+df <- rbind_list(output.list) %>% 
+        select(-c(created_by_id, id, completed, type, created_by_request_id)) %>% 
+          arrange(desc(list_id),desc(starred),desc(revision)) %>% mutate(urgency = "", importance = "")
+df <- data.frame(year = str_sub(df$created_at,1,4), due_date_true = !(df$due_date %>% is.na()), df)
+df$created_at <- as.Date(str_sub(df$created_at,1,10), "%Y-%m-%d")
 
 #export data.frame
 write_table_file(df, name='lista.txt')
